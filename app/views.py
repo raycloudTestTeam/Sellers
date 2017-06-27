e18b31332409ede5f7c2d91bc3908e50992bc9fd
TestTeam <976150787@qq.com>
2017-06-27 17:14:19 +0800

    新增四个页面

--- app/views.py
+++ app/views.py
@@ -7,4 +7,17 @@ from .forms import LoginForm
 @app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title ='用户登录', form = form)
+    return render_template('login.html',
+                           title ='用户登录')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/socket.html')
+def socket_index():
+    return  render_template('socket.html')
+
+@app.route('/sellers.html')
+def seller_index():
+    return render_template('sellers.html')
